6bc49df698f22b162dfaa53ad564f674c6d26f0b
Wesley Steve <wesley.silva23@gotmail.com>
2021-12-09 12:06:28 -0300

    adicionado autor email licença no setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ def read(filename):
 setup(
     name="arquivos_base_criar_pacote_python",
     version="0.1.0",
+    author="Wesley Steve",
+    author_email="wesley.silva23@hotmail.com",
+    maintainer="Wesley Steve",
+    maintainer_email="wesley.silva23@hotmail.com",
+    license="GPL",
     description="arquivos_base_criar_pacote_python",
     packages=find_packages(),
     include_package_data=True,
